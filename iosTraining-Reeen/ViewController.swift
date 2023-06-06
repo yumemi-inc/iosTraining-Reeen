66d7c797aea3f6710e0b2db59cf8904c021cba37
Ren <ren.t21@icloud.com>
2023-06-06 12:07:45 +0900

    Initial Commit

---
+++ iosTraining-Reeen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosTraining-Reeen
+//
+//  Created by 高橋 蓮 on 2023/06/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iosTraining-ReeenUITests/iosTraining_ReeenUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iosTraining_ReeenUITestsLaunchTests.swift
+//  iosTraining-ReeenUITests
+//
+//  Created by 高橋 蓮 on 2023/06/06.
+//
+
+import XCTest
+
+final class iosTraining_ReeenUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
