af749c66c75538881c11d3adf18c6c63cd9fe147
Ren <ren.t21@icloud.com>
2023-06-23 17:20:47 +0900

    feat: sunny以外のテストも追加

--- iosTraining-ReeenTests/iosTraining_ReeenTests.swift
+++ iosTraining-ReeenTests/iosTraining_ReeenTests.swift
@@ -9,10 +9,23 @@ import XCTest
 @testable import iosTraining_Reeen
 
 final class iosTraining_ReeenTests: XCTestCase {
+    let weatherService = YumemiWeatherMock()
 
-    func testWeatherViewController() {
+    func testWeatherConditionImageViewIsSunny() {
         // Arrange
-        let weatherService = YumemiWeatherMock()
+        let weatherViewContoller = WeatherViewController(weatherService: weatherService)
+
+        // Act
+        weatherViewContoller.viewDidLoad()
+        weatherService.getWeatherInformation()
+
+        // Assert
+        let weatherConditionImageView = weatherViewContoller.weatherConditionImageView.image
+        XCTAssertEqual(weatherConditionImageView, UIImage(named: "sunny")?.withTintColor(.red))
+    }
+
+    func testWeatherConditionImageViewIsRainy() {
+        // Arrange
         let weatherViewContoller = WeatherViewController(weatherService: weatherService)
         weatherService.weatherDataMock = WeatherData(maxTemperature: 20, minTemperature: 10, weatherCondition: "rainy")
 
@@ -23,5 +36,19 @@ final class iosTraining_ReeenTests: XCTestCase {
         // Assert
         let weatherConditionImageView = weatherViewContoller.weatherConditionImageView.image
         XCTAssertEqual(weatherConditionImageView, UIImage(named: "rainy")?.withTintColor(.blue))
+    }
+
+    func testWeatherConditionImageViewIsCloudy() {
+        // Arrange
+        let weatherViewContoller = WeatherViewController(weatherService: weatherService)
+        weatherService.weatherDataMock = WeatherData(maxTemperature: 20, minTemperature: 10, weatherCondition: "cloudy")
+
+        // Act
+        weatherViewContoller.viewDidLoad()
+        weatherService.getWeatherInformation()
+
+        // Assert
+        let weatherConditionImageView = weatherViewContoller.weatherConditionImageView.image
+        XCTAssertEqual(weatherConditionImageView, UIImage(named: "cloudy")?.withTintColor(.gray))
     }
 }
