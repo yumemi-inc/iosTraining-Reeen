4a6f27d993c60e488c8d56159eca97147a6a8cdc
Reeen <ren.t21@icloud.com>
2023-07-07 18:33:52 +0900

    feat: reloadButtonの処理をVCから剥がした

--- iosTraining-Reeen/ViewContoller/WeatherViewController.swift
+++ iosTraining-Reeen/ViewContoller/WeatherViewController.swift
@@ -10,6 +10,8 @@ import SnapKit
 
 final class WeatherViewController: UIViewController {
     private let weatherService: WeatherServiceProtocol
+    private var reloadButtonDelegate: ReloadButtonDelegate?
+    private let reloadButtonActionImpl = ReloadButtonActionImpl()
     let weatherView = WeatherView()
 
     init(weatherService: WeatherServiceProtocol) {
@@ -41,15 +43,17 @@ private extension WeatherViewController {
         }
 
         weatherService.delegate = self
+        reloadButtonActionImpl.weatherService = weatherService
+        reloadButtonDelegate = reloadButtonActionImpl
 
         let reloadAction = UIAction { [weak self] _ in
-            self?.weatherService.getWeatherInformation()
+            self?.reloadButtonDelegate?.reloadButtonDidTapped()
         }
 
         weatherView.reloadButton.addAction(reloadAction, for: .touchUpInside)
 
         let closeAction = UIAction { [weak self] _ in
-            self?.closeWeatherViewController()
+            self?.dismiss(animated: true, completion: nil)
         }
         weatherView.closeButton.addAction(closeAction, for: .touchUpInside)
     }
@@ -66,10 +70,6 @@ private extension WeatherViewController {
         if self.presentedViewController != weatherView.errorAlert {
             weatherService.getWeatherInformation()
         }
-    }
-
-    func closeWeatherViewController() {
-        dismiss(animated: true, completion: nil)
     }
 
     func getImage(for condition: String) -> UIImage? {
