09c71237693c531e35471f15a05659b2c81c7eb2
Reeen <ren.t21@icloud.com>
2023-07-28 20:52:52 +0900

    fix: activityIndicatorの処理を見直し

--- iosTraining-Reeen/Model/WeatherService.swift
+++ iosTraining-Reeen/Model/WeatherService.swift
@@ -18,14 +18,8 @@ protocol WeatherServiceDelegate: AnyObject {
 }
 
 final class WeatherService: WeatherServiceProtocol {
-    weak var delegate: WeatherServiceDelegate?
     func getWeatherInformation(completion: @escaping (Result<WeatherData, WeatherError>) -> Void) {
-        DispatchQueue.global(qos: .background).async { [weak self] in
-            guard let self else { return }
-            self.delegate?.weatherServiceDidStartFetching(self)
-            defer {
-                self.delegate?.weatherServiceDidEndFetching(self)
-            }
+        DispatchQueue.global(qos: .background).async {
             do {
                 let encodedRequest = try WeatherEncoder.encodeRequestParameters(WeatherInformationRequest(area: "tokyo", date: Date()))
                 let weatherInfo = try YumemiWeather.syncFetchWeather(encodedRequest)
